2f28df07e7403ceedfc2fa8de3edf28c6c547777
Aristide NIYONGABO <niyongaboaristide17@gmail.com>
2022-11-05 11:32:17 +0200

    created gallery views

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+        'django_filters.rest_framework.DjangoFilterBackend',
+    )
+}
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Gallery, Post
+from .serializers import PostSerializer, GallerySerializer
+
+class GalleryViewSet(viewsets.ModelViewSet):
+
+	queryset = Gallery.objects.none()
+	serializer_class = GallerySerializer
+	permission_classes = [IsAuthenticated,]
+	filter_fields = '__all__'
+	search_fields = ['title',]
+
+	def get_queryset(self):
+		return Gallery.objects.filter(created_by=self.request.user)
+	
+
+class PostViewSet(viewsets.ModelViewSet):
+
+	queryset = Post.objects.none()
+	serializer_class = PostSerializer
+	permission_classes = [IsAuthenticated,]
+	filter_fields = ['id', 'title', 'decription', 'created_at', 'created_by', 'gallery']
+	search_fields = ['title', 'description']
+
+	def get_queryset(self):
+		return Post.objects.filter(created_by=self.request.user)
